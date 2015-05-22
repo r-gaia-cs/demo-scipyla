90a3d8592a693f9e0541b3a9e1d771aa158a9e5c
Raniere Silva <raniere@ime.unicamp.br>
2015-05-22 12:06:48 -0300

    Add documentation and small changes
    Use parenthesis for print call.

--- hello/main.py
+++ hello/main.py
@@ -3,11 +3,12 @@ This is the main file for hello package.
 """
 
 def main():
+    """This is the main function."""
     print("Buenos dias")
     return 0
 
 
 def italian_main():
-    # TODO is this ok? ask the translator guy
-    print "Buona sera!"
-    return 1
+    """This is the italian version."""
+    print("Buona sera!")
+    return 0
