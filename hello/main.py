8c317789f5bce2509cc3224a192374395547af81
Raniere Silva <raniere@ime.unicamp.br>
2015-05-22 12:18:12 -0300

    Fix language selection and add portuguse

--- hello/main.py
+++ hello/main.py
@@ -4,17 +4,23 @@ This is the main file for hello package.
 
 def main(lang="en"):
     """This is the main function."""
-    if lang == "en":
-       english()
+    if lang == "es":
+       spanish()
     elif lang == "it":
        italian()
+    elif lang == "pt":
+       portuguese()
 
     return 0
 
-def english():
+def spanish():
     """This is the english version."""
-    print("Buenos dias")
+    print("Buenos dias!")
 
 def italian():
     """This is the italian version."""
     print("Buona sera!")
+
+def portuguese():
+    """This is the portuguese version."""
+    print("Bom dia!")
